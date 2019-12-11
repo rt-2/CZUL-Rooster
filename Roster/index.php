92d4282e94c0040a789c6d682dcff71c2da045e7
rt-2 <rt-2@rt-2.net>
2019-12-11 10:03:25 -0500

    Update index.php

--- Roster/index.php
+++ Roster/index.php
@@ -4,6 +4,25 @@
 //require_once(dirname(__FILE__).'/CANotAPI.inc.php');
 //require_once(dirname(__FILE__).'/resources/fir.data.inc.php');
 
+// Get cURL resource
+$curl = curl_init();
+// Set some options - we are passing in a useragent too here
+curl_setopt_array($curl, [
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_URL => 'http://testcURL.com',
+    CURLOPT_USERAGENT => 'Codular Sample cURL Request',
+    CURLOPT_POST => 1,
+    CURLOPT_POSTFIELDS => [
+        item1 => 'value',
+        item2 => 'value2'
+    ]
+]);
+// Send the request & save response to $resp
+$resp = curl_exec($curl);
+// Close request to clear up some resources
+curl_close($curl);
+
+
 ?>
 <html>
     <head>
@@ -13,5 +32,9 @@
     <script src="//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 <body>
-    </body>
+	<?php
+	
+	echo $resp;
+	?>
+</body>
 </html>
