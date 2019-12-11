f2cb761425b8da70d577951ebe501584809b6326
rt-2 <rt-2@rt-2.net>
2019-12-11 10:01:32 -0500

    Create index.php

---
+++ Roster/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<?php
+
+//require_once(dirname(__FILE__).'/CANotAPI.inc.php');
+//require_once(dirname(__FILE__).'/resources/fir.data.inc.php');
+
+?>
+<html>
+    <head>
+    <meta charset="UTF-8">
+    <title>Roster</title>
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</head>
+<body>
+    </body>
+</html>
